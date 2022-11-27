2058d2fede15ef46df1af0960d2754fdd424c01f
Rink04 <99864780+Rink04@users.noreply.github.com>
2022-11-27 21:33:02 +0300

    Add files via upload

---
+++ Lab_4/Java.js
@@ -0,0 +1,32 @@
+﻿////Требования к языкам программирования: JAVA, C++, python, assembler
+////Разработать программу на нескольких языках, реализующую следующую математическую функцию в цикле
+// f(i+1) = f(i) + от(n=i)до(100000000) Σ(b * 2 + c - i)
+////и сравнить производительность в зависимости от языка
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(" lab_4, Паршина А Д");
+        int numIterrations = 100000000;
+        long m;
+
+        for (int j = 0; j < 5; j++) {
+            int a = 1, b = 2, c = 3;
+            m = System.currentTimeMillis();
+
+            for (int i = 0; i < numIterrations; i++) {
+                a += b * 2 + c - i;
+            }
+            System.out.println("Ответ:");
+            System.out.println(a);
+            System.out.println("Время");
+            System.out.println((double)(System.currentTimeMillis() - m) * 1.000 + " с");
+        }
+    }
+}
+
+// ответ
+// -187459711
+//32.0 c
+
---
+++ Lab_4/С++.cpp
@@ -0,0 +1,32 @@
+﻿//Требования к языкам программирования : JAVA, C++, python, assembler
+//Разработать программу на нескольких языках, реализующую следующую математическую функцию в цикле
+//f(i + 1) = f(i) + от(n = i)до(100000000) Σ(b * 2 + c - i)
+//и сравнить производительность в зависимости от языка
+
+
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+unsigned int start_time = clock();
+int main()
+{
+    cout << "Lab_4, Паршина А Д " << endl;
+    int a = 0, b = 2, c = 3;
+
+    for (int i = 0; i < 100000000; i++) {
+        a += 2 * b + c - i;
+    }
+
+    cout << "Ответ:" << a << endl;
+    unsigned int end_time = clock();
+    unsigned int search_time = end_time - start_time;
+    cout << "Время: " << search_time << " мс" << endl;
+    return 0;
+}
+
+ 
+// ответ
+// -187459712
+// 223150 мс
